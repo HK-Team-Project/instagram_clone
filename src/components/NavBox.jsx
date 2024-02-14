1f10c44021366173a2d596f5b2531d5f8c610a0c
Byunghee Kang <byungheekang@g.skku.edu>
2024-02-14 13:21:21 +0900

    nav css 임시 처리 완료
    navigation bar의 element를 컴포넌트로 분리 후 props로 이름, 이미지, 경로 설정

--- src/components/NavBox.jsx
+++ src/components/NavBox.jsx
@@ -1,8 +1,47 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+function capitalizeFirstLetter(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export default function NavBox({ name, iconSrc, path }) {
   return (
-    <div>
-      <img src={iconSrc} alt="" style={{ width: "10%" }} />
-      <span>{name}</span>
-    </div>
+    <Container>
+      <StyledLink to={path}>
+        <Img src={iconSrc} alt="" />
+        <Text>{capitalizeFirstLetter(name)}</Text>
+      </StyledLink>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: inline-block;
+  margin: 12px 0 12px 0;
+`;
+
+const StyledLink = styled(Link)`
+  display:flex;
+  flex-direction: row;
+  text-decoration: none;
+`;
+
+const Img = styled.img`
+  width: 12%;
+  margin-left:12px;
+  margin-right:12 px;
+  @media screen and (max-width: 1275px){
+    width: 80%;
+  }
+`;
+
+const Text = styled.span`
+  margin-left: 10px;
+  text-align:center;
+  font-size:20px;
+  color:black;
+  @media screen and (max-width: 1275px){
+    display: none;
+  }
+`
